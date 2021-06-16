e7c22686979581f570fe4e005d564451e745a6c7
Carlos Accyole <carlos_accyole@hotmail.com>
2021-06-16 13:00:21 -0300

    Criando e Listando Séries

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => '/api'], function () use ($router) {
-    return $router->get('/series', 'SeriesController@index');
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('series', 'SeriesController@store');
+    $router->get('series', 'SeriesController@index');
+    $router->get('series/{id}', 'SeriesController@get');
 });
