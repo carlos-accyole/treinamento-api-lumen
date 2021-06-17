5f15d6f9f753c48f7609f77aa88782725d4ccce6
Carlos Accyole <carlos_accyole@hotmail.com>
2021-06-16 23:45:50 -0300

    Editando uma série

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -14,10 +14,14 @@ class SeriesController
 
     public function store(Request $request)
     {
-         return response()->json(Serie::create(['nome' => $request->nome]), 201);
+         return response()
+             ->json(
+                 Serie::create($request->all()),
+                 201
+             );
     }
 
-    public function get(int $id)
+    public function show(int $id)
     {
         $serie = Serie::find($id);
 
@@ -26,5 +30,22 @@ class SeriesController
         }
 
         return response()->json($serie);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $serie = Serie::find($id);
+
+        if (is_null($serie)) {
+            return response()->json([
+                'erro' => 'Recurso não encontrado'
+            ],
+            404);
+        }
+
+        $serie->fill($request->all());
+        $serie->save();
+
+        return $serie;
     }
 }
