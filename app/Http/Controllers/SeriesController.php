ddd509330c039f179ab9f3bfba4c070353dc7be6
Carlos Accyole <carlos_accyole@hotmail.com>
2021-06-17 02:03:50 -0300

    Criando o controller abstrato

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,64 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Serie;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
-class SeriesController
+class SeriesController extends BaseController
 {
-    public function index()
-    {
-        return Serie::all();
-    }
-
-    public function store(Request $request)
-    {
-         return response()
-             ->json(
-                 Serie::create($request->all()),
-                 201
-             );
-    }
-
-    public function show(int $id)
-    {
-        $serie = Serie::find($id);
-
-        if (is_null($serie)) {
-            return response()->json('', 204);
-        }
-
-        return response()->json($serie);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        $serie = Serie::find($id);
-
-        if (is_null($serie)) {
-            return response()->json([
-                'erro' => 'Recurso não encontrado'
-            ],
-            404);
-        }
-
-        $serie->fill($request->all());
-        $serie->save();
-
-        return $serie;
-    }
-
-    public function destroy(int $id)
-    {
-        $qtdDeSeriesRemovidas = Serie::destroy($id);
-
-        if ($qtdDeSeriesRemovidas === 0) {
-            return response()->json([
-                'erro' => 'Recurso não encontrado'
-            ],
-           404);
-        }
-
-        return response()->json('', 204);
-    }
+   public function __construct()
+   {
+       $this->classe = Serie::class;
+   }
 }
